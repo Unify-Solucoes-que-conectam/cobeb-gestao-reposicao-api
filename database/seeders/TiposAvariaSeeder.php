e02afeaf2442578d1f4ee324d6459280211c3bd9
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-07-17 01:53:59 -0300

    feat: adicionar campo "nome" em tiposAvaria

--- database/seeders/TiposAvariaSeeder.php
+++ database/seeders/TiposAvariaSeeder.php
@@ -15,14 +15,24 @@ class TiposAvariaSeeder extends Seeder
         $usuario_responsavel_id = config('auth.default_sys_uuid', '4be3c49f-7fe4-45db-a3b4-e80cf45e9247');
 
         $tiposAvaria = [
-            'Avariado',
-            'Faltante',
-            'Inversão',
+            [
+                'nome' => 'Avariado',
+                'descricao' => 'Quebra, produto mal-cheio ou furado',
+            ],
+            [
+                'nome' => 'Inversão',
+                'descricao' => 'Produtos faltantes, carga incompleta',
+            ],
+            [
+                'nome' => 'Faltante',
+                'descricao' => 'Troca de produtos a serem entregues',
+            ],
         ];
 
-        foreach ($tiposAvaria as $descricao) {
+        foreach ($tiposAvaria as $tipoAvaria) {
             TipoAvaria::create([
-                'descricao' => $descricao,
+                'nome' => $tipoAvaria['nome'],
+                'descricao' => $tipoAvaria['descricao'],
                 'usuario_responsavel_id' => $usuario_responsavel_id
             ]);
         }
